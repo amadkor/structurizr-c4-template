59130feba136646ffadeb2abcf66b2271e806f26
Ahmed Madkor <ahmed.madkor@monstar-lab.com>
2023-11-13 14:24:06 +0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start docker run -it --rm -p 8081:8080 -v/mnt/c/Users/amadkor/repos/WSEC-C4/:/usr/local/structurizr structurizr/lite
+start docker run -it --rm -p 8081:8080 -v%cd%:/usr/local/structurizr structurizr/lite
 start http://localhost:8081
